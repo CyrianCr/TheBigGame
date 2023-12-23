164b8b4c57321967a3c3a6e4e715faeb707a1964
CyrianCr <ax008952@acsmail.ucsd.edu>
2023-12-22 18:40:45 -0800

    7

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ let choix3 = document.getElementById("choix3")
 let choix4 = document.getElementById("choix4")
 let choix5 = document.getElementById("choix5")
 let choix6 = document.getElementById("choix6")
+let choix7 = document.getElementById("choix7")
+let choix8 = document.getElementById("choix8")
+let choix9 = document.getElementById("choix9")
+let choix10 = document.getElementById("choix10")
 
 let out = document.getElementById("sortie")
 
@@ -67,4 +71,42 @@ choix6.addEventListener("click", function() {
         audio.play();}
     });
 
+choix7.addEventListener("click", function() {
+    if (choix7.getAttribute("class") === "valid") {
+        choix7.style.backgroundColor = "green"}
+    else if (choix7.getAttribute("class") === "false") {
+        choix7.style.backgroundColor = "red";
+        out.innerHTML = "BOUUUUUUUH GROS BEBE";
+        var audio = new Audio('./img/prout.mp3');
+        audio.play();}
+    });
 
+choix8.addEventListener("click", function() {
+    if (choix8.getAttribute("class") === "valid") {
+        choix8.style.backgroundColor = "green"}
+    else if (choix8.getAttribute("class") === "false") {
+        choix8.style.backgroundColor = "red";
+        out.innerHTML = "BOUUUUUUUH GROS BEBE";
+        var audio = new Audio('./img/prout.mp3');
+        audio.play();}
+    });
+
+choix9.addEventListener("click", function() {
+    if (choix9.getAttribute("class") === "valid") {
+        choix9.style.backgroundColor = "green"}
+    else if (choix9.getAttribute("class") === "false") {
+        choix9.style.backgroundColor = "red";
+        out.innerHTML = "BOUUUUUUUH GROS BEBE";
+        var audio = new Audio('./img/prout.mp3');
+        audio.play();}
+});
+
+choix10.addEventListener("click", function() {
+    if (choix10.getAttribute("class") === "valid") {
+        choix10.style.backgroundColor = "green"}
+    else if (choix10.getAttribute("class") === "false") {
+        choix10.style.backgroundColor = "red";
+        out.innerHTML = "BOUUUUUUUH GROS BEBE";
+        var audio = new Audio('./img/prout.mp3');
+    audio.play();}
+});
